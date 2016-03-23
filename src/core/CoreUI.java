52a9343d376f03127a9d9701ec0a69b3db9f395e
Ross <rossgithub@gmail.com>
2016-03-23 00:22:17 +0000

    WIP - Implementing User Inteface
    Added all of the buttons and text fields

--- src/core/CoreUI.java
+++ src/core/CoreUI.java
@@ -14,6 +14,7 @@ public class CoreUI extends PApplet {
 	/* Details about the environment the robot is in */
 	int Grid_Size_MM = 300;
 	int Wall_Distance_MM = 180;
+	int Num_Grid_Cells = 5;
 	float Wall_Threshold_Percent = 1.2f;
 	
 	Direction Side_Doors_Are_On;
@@ -22,84 +23,27 @@ public class CoreUI extends PApplet {
 	/* Detail about the robot */
 	Robot robot;
 	
-	/* Details about the chart to be displayed and general UI */
-	BarChart barChart;
-	int barChartWidth;
-	int barChartHeight;
-	int barChartX;
-	int barChartY;
 	
-	ControlP5 cp5;
-	PFont ArialFont20;
-	
-	ButtonBar controlBtnBar;
-	int controlBtnBarHeight;
-	int controlBtnBarWidth;
-	int controlBtnBarX;
-	int controlBtnBarY;
-	
-	Textfield controlValue;
-	int controlValueHeight;
-	int controlValueWidth;
-	int controlValueX;
-	int controlValueY;
 	
 	/* ------------------------------------------------------ */
 	
 	public void settings() {
 		
-		size(1200, 700);
+		size(1200, 800);
 		
 	}
 	
 	public void setup() {
 		
 		
-		// UI setup
-		cp5 = new ControlP5(this);
-		ArialFont20 = createFont("arial", 20);
-		
-		
-		// Bar chart  setup
-		barChart = new BarChart(this);
-		barChart.setMinValue(0.0f);
-		barChart.setMaxValue(1.0f);
-		barChart.showValueAxis(true);
-		barChart.showCategoryAxis(true);
-		barChart.setCategoryAxisLabel("Grid Cells");
-		
-		barChartWidth = width-400;
-		barChartHeight = height -200;
-		barChartX = 50;
-		barChartY = 50;
-		
-		controlValueHeight = 50;
-		controlValueWidth = barChartWidth/8;
-		controlValueX = barChartX;
-		controlValueY = barChartHeight + 100;
-		
-		controlValue = cp5.addTextfield("Value")
-				.setPosition(controlValueX, controlValueY)
-				.setSize(controlValueWidth, controlValueHeight)
-				.setFont(ArialFont20)
-				.setColor(this.color(255, 255, 255));
-		
-		controlBtnBarHeight = 50;
-		controlBtnBarWidth = (barChartWidth/8) * 7 ;
-		controlBtnBarX = controlValueX + controlValueWidth + 20;
-		controlBtnBarY = barChartHeight + 100;
-		
-		controlBtnBar = cp5.addButtonBar("controlBtnBar")
-				 .setPosition(controlBtnBarX, controlBtnBarY)
-			 	 .setSize(controlBtnBarWidth, controlBtnBarHeight)
-			 	 .addItems(split("Move Left, Sense, Move Right", ","));
+		setupUserInterface();
 		
 		setupListeners(); 
 		
 		//Robot robot = new Robot(this, Grid_Size_MM, Wall_Distance_MM, Wall_Threshold_Percent, serialData);
 		//robot.connect("/dev/tty.HC-05-DevB", 9600);
 		int[] doors = {1, 2};
-		Map map = new Map(5, doors);
+		Map map = new Map(Num_Grid_Cells, doors);
 		System.out.println(map);
 		
 		map.setSensorModel(0.9, 0.1);
@@ -124,6 +68,263 @@ public class CoreUI extends PApplet {
 	
 	public void postRobotSensing (boolean result) {
 		
+	}
+	
+	public void setupUserInterface() {
+		
+		/* UI setup ------------------------------------------------------------------- */
+		cp5 = new ControlP5(this);
+		ArialFont20 = createFont("arial", 20, true);
+		ArialFont14 = createFont("arial", 14, true);
+		
+		
+		/* Bar chart  setup ----------------------------------------------------------- */
+		barChart = new BarChart(this);
+		barChart.setMinValue(0.0f);
+		barChart.setMaxValue(1.0f);
+		barChart.showValueAxis(true);
+		barChart.showCategoryAxis(true);
+		barChart.setCategoryAxisLabel("Grid Cells");
+		
+		barChartWidth = width-400;
+		barChartHeight = height -200;
+		barChartX = 50;
+		barChartY = 50;
+		
+		/* control value field -------------------------------------------------------- */
+		controlValueHeight = 50;
+		controlValueWidth = barChartWidth/8;
+		controlValueX = barChartX;
+		controlValueY = barChartHeight + 100;
+		controlValue = cp5.addTextfield("Value")
+				.setPosition(controlValueX, controlValueY)
+				.setSize(controlValueWidth, controlValueHeight)
+				.setFont(ArialFont20)
+				.setCaptionLabel("Grid Spaces")
+				.setColorCaptionLabel(0)
+				.setColor(this.color(255, 255, 255));
+		
+		controlValue.getCaptionLabel()
+					.setSize(14);
+		
+		/* control button bar at bottom ----------------------------------------------- */
+		controlBtnBarHeight = 50;
+		controlBtnBarWidth = (barChartWidth/8) * 7 ;
+		controlBtnBarX = controlValueX + controlValueWidth + 20;
+		controlBtnBarY = barChartHeight + 100;
+		
+		controlBtnBar = cp5.addButtonBar("controlBtnBar")
+				 .setPosition(controlBtnBarX, controlBtnBarY)
+			 	 .setSize(controlBtnBarWidth, controlBtnBarHeight)
+			 	 .addItems(split("Move Left, Sense, Move Right", ","));
+		
+		int spaceBetweenChartAndEnd = width - (barChartX + barChartWidth);
+		int standardFieldHeight = 35;
+		int standardFieldWidth = (spaceBetweenChartAndEnd / 3) * 2;
+		
+		/* connection port field ----------------------------------------------------- */
+		connectionPortHeight = standardFieldHeight;
+		connectionPortWidth = (standardFieldWidth / 3) * 2;
+		connectionPortX = (barChartX + barChartWidth) + 100;
+		connectionPortY = (height / 20) * 1;
+		connectionPort = cp5.addTextfield("connectionPort")
+							.setPosition(connectionPortX, connectionPortY)
+							.setSize(connectionPortWidth, connectionPortHeight)
+							.setFont(ArialFont14)
+							.setCaptionLabel("Connection Port")
+							.setColorCaptionLabel(0)
+							.setColor(this.color(255, 255, 255));
+		
+		connectionPort.getCaptionLabel()
+					  .setSize(14);
+		
+		/* connect button ------------------------------------------------------------ */
+		connectButtonHeight = standardFieldHeight;
+		connectButtonWidth = standardFieldWidth;
+		connectButtonX = (barChartX + barChartWidth) + 100;
+		connectButtonY = ((height / 20) * 19) - 30;
+		connectButton = cp5.addButton("connectButton")
+					   .setPosition(connectButtonX, connectButtonY)
+					   .setSize(connectButtonWidth, connectButtonHeight)
+					   .setCaptionLabel("Connect");
+		
+		connectButton.getCaptionLabel()
+					  .setSize(14);
+		
+		/* world size field ---------------------------------------------------------- */
+		worldSizeHeight = standardFieldHeight;
+		worldSizeWidth = (standardFieldWidth/2) - 10;
+		worldSizeX = (barChartX + barChartWidth) + 100;
+		worldSizeY = (height / 20) * 3;
+		worldSize = cp5.addTextfield("worldSize")
+					   .setPosition(worldSizeX, worldSizeY)
+					   .setSize(worldSizeWidth, worldSizeHeight)
+					   .setFont(ArialFont20)
+					   .setCaptionLabel("World Size")
+					   .setColorCaptionLabel(0)
+					   .setColor(this.color(255, 255, 255));
+		
+		worldSize.getCaptionLabel()
+					  .setSize(14);
+		
+		/* door positions field ------------------------------------------------------ */
+		doorPositionsHeight = standardFieldHeight;
+		doorPositionsWidth = (standardFieldWidth/2) - 10;;
+		doorPositionsX = worldSizeX + worldSizeWidth + 20;
+		doorPositionsY = (height / 20) * 3;
+		doorPositions = cp5.addTextfield("doorPositions")
+					   .setPosition(doorPositionsX, doorPositionsY)
+					   .setSize(doorPositionsWidth, doorPositionsHeight)
+					   .setFont(ArialFont20)
+					   .setCaptionLabel("Door Positions")
+					   .setColorCaptionLabel(0)
+					   .setColor(this.color(255, 255, 255));
+		
+		doorPositions.getCaptionLabel()
+					  .setSize(14);
+		
+		/* is world cyclic check box ------------------------------------------------- */
+		cyclicWorldHeight = standardFieldHeight;
+		cyclicWorldWidth = standardFieldWidth/4;
+		cyclicWorldX = (barChartX + barChartWidth) + 100;
+		cyclicWorldY = (height / 20) * 5;
+		cyclicWorld = cp5.addRadioButton("cyclicWorld")
+						   .setPosition(cyclicWorldX, cyclicWorldY)
+						   .setItemsPerRow(2)
+						   .setSize(cyclicWorldWidth, cyclicWorldHeight)
+						   .setCaptionLabel("Is World Cyclic?")
+						   .setColorLabel(color(0))
+						   .setSpacingColumn(cyclicWorldWidth)
+						   .addItem("Cyclic", 1)
+						   .addItem("Non Cyclic", 0);
+		
+		cyclicWorld.getItem(0)
+				   .getCaptionLabel()
+				   .setSize(12)
+				   .setColor(0, true);
+		
+		cyclicWorld.getItem(1)
+		   		   .getCaptionLabel()
+		   		   .setSize(12)
+		   		   .setColor(0, true);
+		
+		/* sensor hit field --------------------------------------------------------- */
+		senseHitProductHeight = standardFieldHeight;
+		senseHitProductWidth = standardFieldWidth;
+		senseHitProductX = (barChartX + barChartWidth) + 100;
+		senseHitProductY = (height / 20) * 7;
+		senseHitProduct = cp5.addTextfield("senseHitProduct")
+					   .setPosition(senseHitProductX, senseHitProductY)
+					   .setSize(senseHitProductWidth, senseHitProductHeight)
+					   .setFont(ArialFont20)
+					   .setCaptionLabel("Sensor Hit Product")
+					   .setColorCaptionLabel(0)
+					   .setColor(this.color(255, 255, 255));
+		
+		senseHitProduct.getCaptionLabel()
+					  .setSize(14);
+		
+		/* sensor miss field ------------------------------------------------------- */
+		senseMissProductHeight = standardFieldHeight;
+		senseMissProductWidth = standardFieldWidth;
+		senseMissProductX = (barChartX + barChartWidth) + 100;
+		senseMissProductY = (height / 20) * 9;
+		senseMissProduct = cp5.addTextfield("senseMissProduct")
+					   .setPosition(senseMissProductX, senseMissProductY)
+					   .setSize(senseMissProductWidth, senseMissProductHeight)
+					   .setFont(ArialFont20)
+					   .setCaptionLabel("Sensor Miss Product")
+					   .setColorCaptionLabel(0)
+					   .setColor(this.color(255, 255, 255));
+		
+		senseMissProduct.getCaptionLabel()
+					  .setSize(14);
+		
+		/* probability exact motion ------------------------------------------------ */
+		probExactMotionHeight = standardFieldHeight;
+		probExactMotionWidth = standardFieldWidth;
+		probExactMotionX = (barChartX + barChartWidth) + 100;
+		probExactMotionY = (height / 20) * 11;
+		probExactMotion = cp5.addTextfield("probExactMotion")
+					   .setPosition(probExactMotionX, probExactMotionY)
+					   .setSize(probExactMotionWidth, probExactMotionHeight)
+					   .setFont(ArialFont20)
+					   .setCaptionLabel("Probability Exact Motion")
+					   .setColorCaptionLabel(0)
+					   .setColor(this.color(255, 255, 255));
+		
+		probExactMotion.getCaptionLabel()
+					  .setSize(14);
+		
+		/* probability motion overshoot -------------------------------------------- */
+		probInexactMotionOverHeight = standardFieldHeight;
+		probInexactMotionOverWidth = standardFieldWidth;
+		probInexactMotionOverX = (barChartX + barChartWidth) + 100;
+		probInexactMotionOverY = (height / 20) * 13;
+		probInexactMotionOver = cp5.addTextfield("probInexactMotionOver")
+					   .setPosition(probInexactMotionOverX, probInexactMotionOverY)
+					   .setSize(probInexactMotionOverWidth, probInexactMotionOverHeight)
+					   .setFont(ArialFont20)
+					   .setCaptionLabel("Probability Motion Overshoot")
+					   .setColorCaptionLabel(0)
+					   .setColor(this.color(255, 255, 255));
+		
+		probInexactMotionOver.getCaptionLabel()
+					  .setSize(14);
+		
+		/* probability motion undershoot ------------------------------------------- */
+		probInexactMotionUnderHeight = standardFieldHeight;
+		probInexactMotionUnderWidth = standardFieldWidth;
+		probInexactMotionUnderX = (barChartX + barChartWidth) + 100;
+		probInexactMotionUnderY = (height / 20) * 15;
+		probInexactMotionUnder = cp5.addTextfield("probInexactMotionUnder")
+					   .setPosition(probInexactMotionUnderX, probInexactMotionUnderY)
+					   .setSize(probInexactMotionUnderWidth, probInexactMotionUnderHeight)
+					   .setFont(ArialFont20)
+					   .setCaptionLabel("Probability Motion Undershoot")
+					   .setColorCaptionLabel(0)
+					   .setColor(this.color(255, 255, 255));
+		
+		probInexactMotionUnder.getCaptionLabel()
+					  .setSize(14);
+		
+		/* door side check box ----------------------------------------------------- */
+		doorSideHeight = standardFieldHeight;
+		doorSideWidth = standardFieldWidth/4;
+		doorSideX = (barChartX + barChartWidth) + 100;
+		doorSideY = (height / 20) * 17;
+		doorSide = cp5.addRadioButton("		doorSide")
+						   .setPosition(doorSideX, doorSideY)
+						   .setItemsPerRow(2)
+						   .setSize(doorSideWidth, doorSideHeight)
+						   .setCaptionLabel("Door Side")
+						   .setColorLabel(color(0))
+						   .setSpacingColumn(cyclicWorldWidth)
+						   .addItem("Left", 1)
+						   .addItem("Right", 0);
+				
+		doorSide.getItem(0)
+			    .getCaptionLabel()
+			    .setSize(12)
+			    .setColor(0, true);
+				
+		doorSide.getItem(1)
+	   		    .getCaptionLabel()
+	   		    .setSize(12)
+	   		    .setColor(0, true);
+		
+		/* set button -------------------------------------------------------------- */
+		setButtonHeight = standardFieldHeight;
+		setButtonWidth = standardFieldWidth;
+		setButtonX = (barChartX + barChartWidth) + 100;
+		setButtonY = ((height / 20) * 19) - 30;
+		setButton = cp5.addButton("setButton")
+					   .setPosition(setButtonX, setButtonY)
+					   .setSize(setButtonWidth, setButtonHeight)
+					   .setCaptionLabel("Set");
+		
+		setButton.getCaptionLabel()
+					  .setSize(14);
 	}
 	
 	public void setupListeners() {
@@ -221,5 +422,100 @@ public class CoreUI extends PApplet {
 			}
 		}
 	}
-
+	
+	/* User Interface Variables */
+	/* Details about the chart to be displayed and general UI */
+	BarChart barChart;
+	int barChartWidth;
+	int barChartHeight;
+	int barChartX;
+	int barChartY;
+	
+	ControlP5 cp5;
+	PFont ArialFont20;
+	PFont ArialFont14;
+	
+	ButtonBar controlBtnBar;
+	int controlBtnBarHeight;
+	int controlBtnBarWidth;
+	int controlBtnBarX;
+	int controlBtnBarY;
+	
+	Textfield controlValue;
+	int controlValueHeight;
+	int controlValueWidth;
+	int controlValueX;
+	int controlValueY;
+	
+	Textfield connectionPort;
+	int connectionPortHeight;
+	int connectionPortWidth;
+	int connectionPortX;
+	int connectionPortY;
+	
+	Button connectButton;
+	int connectButtonHeight;
+	int connectButtonWidth;
+	int connectButtonX;
+	int connectButtonY;
+	
+	Textfield worldSize;
+	int worldSizeHeight;
+	int worldSizeWidth;
+	int worldSizeX;
+	int worldSizeY;
+	
+	Textfield doorPositions;
+	int doorPositionsHeight;
+	int doorPositionsWidth;
+	int doorPositionsX;
+	int doorPositionsY;
+	
+	RadioButton cyclicWorld;
+	int cyclicWorldHeight;
+	int cyclicWorldWidth;
+	int cyclicWorldX;
+	int cyclicWorldY;
+	
+	Textfield senseHitProduct;
+	int senseHitProductHeight;
+	int senseHitProductWidth;
+	int senseHitProductX;
+	int senseHitProductY;
+	
+	Textfield senseMissProduct;
+	int senseMissProductHeight;
+	int senseMissProductWidth;
+	int senseMissProductX;
+	int senseMissProductY;
+	
+	Textfield probExactMotion;
+	int probExactMotionHeight;
+	int probExactMotionWidth;
+	int probExactMotionX;
+	int probExactMotionY;
+	
+	Textfield probInexactMotionOver;
+	int probInexactMotionOverHeight;
+	int probInexactMotionOverWidth;
+	int probInexactMotionOverX;
+	int probInexactMotionOverY;
+	
+	Textfield probInexactMotionUnder;
+	int probInexactMotionUnderHeight;
+	int probInexactMotionUnderWidth;
+	int probInexactMotionUnderX;
+	int probInexactMotionUnderY;
+	
+	RadioButton doorSide;
+	int doorSideHeight;
+	int doorSideWidth;
+	int doorSideX;
+	int doorSideY;
+	
+	Button setButton;
+	int setButtonHeight;
+	int setButtonWidth;
+	int setButtonX;
+	int setButtonY;
 }
